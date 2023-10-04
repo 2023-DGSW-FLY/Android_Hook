3b58b10852619ca6c39625e6d453dd104eb107d4
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-10-04 22:19:00 +0900

    📝:: create Dummy data

---
+++ data/src/main/java/com/innosync/data/repository/model/JobOfferModel.kt
@@ -0,0 +1,9 @@
+package com.innosync.data.repository.model
+
+data class JobOfferModel(
+    val userName: String,
+    val competitionName: String,
+    val technology: String,
+    val time: String,
+    val img: Int
+)
