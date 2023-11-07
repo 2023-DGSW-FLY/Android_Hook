b6679ed1b22979e9b6b213ea4ff63c6cb1c39e62
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-07 18:27:33 +0900

    📝:: Create ProfileFix Module
    프로필 수정 모듈

--- di/src/main/java/com/innosync/di/RepositoryModule.kt
+++ di/src/main/java/com/innosync/di/RepositoryModule.kt
@@ -9,6 +9,7 @@ import com.innosync.data.repository.AuthRepositoryImpl
 import com.innosync.data.repository.ChatRepositoryImpl
 import com.innosync.data.repository.FirebaseTokenRepositoryImpl
 import com.innosync.data.repository.AlarmRepositoryImpl
+import com.innosync.data.repository.ProfileRepositoryImpl
 import com.innosync.data.repository.TokenRepositoryImpl
 import com.innosync.data.repository.UserRepositoryImpl
 import com.innosync.domain.repository.CongressRepository
@@ -20,6 +21,7 @@ import com.innosync.domain.repository.AuthRepository
 import com.innosync.domain.repository.ChatRepository
 import com.innosync.domain.repository.FirebaseTokenRepository
 import com.innosync.domain.repository.AlarmRepository
+import com.innosync.domain.repository.ProfileFixRepository
 import com.innosync.domain.repository.TokenRepository
 import com.innosync.domain.repository.UserRepository
 import dagger.Binds
@@ -97,4 +99,12 @@ abstract class RepositoryModule {
     abstract fun providesSharedPreferencesRepository(
         alarmRepositoryImpl: AlarmRepositoryImpl
     ): AlarmRepository
+
+
+
+    @Singleton
+    @Binds
+    abstract fun providesProfileRepository(
+        profileRepositoryImpl: ProfileRepositoryImpl
+    ): ProfileFixRepository
 }
