f4ba6eca17bbbca5e5e425d982228543044d3f14
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-07 18:26:52 +0900

    📝:: Add Profile  function
    프로필 기능 추가

--- presentation/src/main/java/com/innosync/hook/feature/profile/ProfileFragment.kt
+++ presentation/src/main/java/com/innosync/hook/feature/profile/ProfileFragment.kt
@@ -1,25 +1,63 @@
 package com.innosync.hook.feature.profile
 
+
 import android.util.Log
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.innosync.hook.R
 import com.innosync.hook.base.BaseFragment
 import com.innosync.hook.databinding.FragmentProfileBinding
+import com.innosync.hook.feature.loading.LoadingDialog
 import com.innosync.hook.feature.profile.ProfileViewModel.Companion.ON_CLICK_CORRECTION
+import com.innosync.hook.feature.profile.ProfileViewModel.Companion.ON_FAILURE
+import com.innosync.hook.feature.profile.ProfileViewModel.Companion.ON_SUCCESS
+import com.innosync.hook.util.toImageUrl
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.log
+
 
 @AndroidEntryPoint
 class ProfileFragment:BaseFragment<FragmentProfileBinding, ProfileViewModel>() {
     override val viewModel: ProfileViewModel by viewModels()
 
-    override fun observerViewModel() {
+    private var loadingDialog: LoadingDialog? = null
+
+    override fun onStart() {
+
+        super.onStart()
         observeData()
         viewModel.loadInfo()
+
+    }
+
+    override fun observerViewModel() {
+        Log.d("TAG", "observerViewModel: profile")
+
+        if (loadingDialog == null) {
+            loadingDialog = LoadingDialog(requireContext())
+        }
+        loadingDialog!!.show()
+
         bindingViewEvent { event ->
             when(event){
                 ON_CLICK_CORRECTION ->{
-                    Log.d("태그", "observerViewModel: 수정하기 버튼")
+                    Log.d("TAG", "observerViewModel: ")
+                    findNavController().navigate(
+                        R.id.action_profile_to_profileFix
+                    )
                 }
+
+                ON_SUCCESS->{
+                    if (loadingDialog!=null && loadingDialog!!.isShowing){
+                        loadingDialog!!.dismiss()
+                    }
+                }
+
+                ON_FAILURE ->{
+
+                }
+
             }
         }
     }
@@ -33,6 +71,13 @@ class ProfileFragment:BaseFragment<FragmentProfileBinding, ProfileViewModel>() {
                 portfolioUrl.text = it.portfolioURL
                 githubUrl.text = it.githubURL
                 userInfo.text = it.userInfo
+                Glide.with(this@ProfileFragment)
+                    .load(it.id.toString().toImageUrl())
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .skipMemoryCache(true)
+                    .into(profileImage)
+                Log.d("TAG", "observeData: ${it.id.toString().toImageUrl()}")
             }
         }
     }
--- presentation/src/main/java/com/innosync/hook/feature/profile/ProfileViewModel.kt
+++ presentation/src/main/java/com/innosync/hook/feature/profile/ProfileViewModel.kt
@@ -9,6 +9,8 @@ import com.innosync.hook.base.BaseViewModel
 import com.innosync.hook.util.launchIO
 import com.innosync.hook.util.launchMain
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,14 +21,19 @@ class ProfileViewModel @Inject constructor(
     private val _profileData = MutableLiveData<UserModel>()
     val profileData : LiveData<UserModel> = _profileData
 
+
     fun loadInfo() = launchIO {
         userGetInfoUseCase.invoke()
             .onSuccess {
                 launchMain {
                     _profileData.value = it
+                    viewEvent(ON_SUCCESS)
                 }
+
             }.onFailures {
                 Log.d("TAG", "loadInfo: $it")
+                viewEvent(ON_FAILURE)
+
             }
     }
 
@@ -36,5 +43,9 @@ class ProfileViewModel @Inject constructor(
 
     companion object{
         const val ON_CLICK_CORRECTION = 1
+        const val ON_SUCCESS = 2
+        const val ON_FAILURE = 3
     }
 }
+
+
