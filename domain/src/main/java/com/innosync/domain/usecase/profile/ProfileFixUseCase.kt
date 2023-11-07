1abcec12a738edaf9c1108240f2a015cbdf5f356
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-11-07 18:29:24 +0900

    📝:: Create Profile Fix
    프로필 수정하는 기능을 만듬

---
+++ domain/src/main/java/com/innosync/domain/usecase/profile/ProfileFixUseCase.kt
@@ -0,0 +1,34 @@
+package com.innosync.domain.usecase.profile
+
+import android.graphics.Bitmap
+import android.util.Log
+import com.innosync.domain.repository.ProfileFixRepository
+import javax.inject.Inject
+
+class ProfileFixUseCase @Inject constructor(
+    private val profileRepository: ProfileFixRepository
+) {
+
+
+    suspend operator fun invoke(
+        userAccount: String,
+        userName: String,
+        email: String,
+        userInfo: String,
+        githubURL: String,
+        portfolioURL: String,
+        profileImage: Bitmap?
+        )= kotlin.runCatching {
+        Log.d("TAG", "invoke: ")
+            profileRepository.Fix(
+                userAccount = userAccount,
+                userName = userName,
+                email = email,
+                userInfo = userInfo,
+                githubURL = githubURL,
+                portfolioURL = portfolioURL,
+                profileImage = profileImage
+            )
+
+    }
+}
